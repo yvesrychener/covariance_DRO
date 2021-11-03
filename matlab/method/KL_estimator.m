5099d32f146bc3dcce02d09811d8a0e2b4d0bd46
Yves Rychener <yves.rychener@epfl.ch>
2021-11-03 09:39:56 +0100

    Updated KL in matlab

--- matlab/method/KL_estimator.m
+++ matlab/method/KL_estimator.m
@@ -4,8 +4,8 @@ function [sigma_star] = KL_estimator(sigma_hat,epsilon)
 vals = diag(vals);
 p = length(vals);
 sigma_max = max(vals);
-interval = [0, 2*sigma_max^2*exp(-4*epsilon/p)/(1-exp(-2*epsilon/p))];
-X = @(dualvar) 2./(1+sqrt(1+8.*vals.^2./dualvar));
+interval = [0, 4*sigma_max^2*exp(-4*epsilon/p)/(1-exp(-2*epsilon/p))];
+X = @(dualvar) 2./(1+sqrt(1+16.*vals.^2./dualvar));
 
 obj = @(dualvar) 2*epsilon + p + sum(log(X(dualvar))-X(dualvar));
 
--- matlab/method/KL_sigmas.m
+++ matlab/method/KL_sigmas.m
@@ -2,6 +2,6 @@ function [sigma_stars] = KL_sigmas(sigmas,dualvar)
 %KL_SIGMAS Calculates the covariance eigenvalues for KL
 %   sigmas: eigenvalues of sample covariance matrix
 %   dualvr: optimal gamma
-    sigma_stars = (sqrt((dualvar./sigmas).^2+8*dualvar)-dualvar./sigmas)./4;
+    sigma_stars = (sqrt(dualvar^2+16*dualvar.*sigmas.^2)-dualvar)./(8*sigmas);
 end
 
